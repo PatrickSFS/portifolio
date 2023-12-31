7c181a82860a765fe299d9c53ffb98a8cfb5bfe0
patrisfs <115261085+PatrickSFS@users.noreply.github.com>
2023-12-30 23:06:50 -0300

    Feat: criando estrutura principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Header from "./components/Header";
+import Footer from './components/Footer';
+import Main from "./components/Main";
+import "./App.css";
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="app">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   )
 }
 
